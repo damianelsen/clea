ef16b27460f79568833b9022fcae38dc7a15f87d
Damian Elsen <damian.elsen@googlemail.com>
2018-07-19 09:26:56 -0500

    Initial Commit

---
+++ Clea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clea
+//
+//  Created by Damian Elsen on 7/19/18.
+//  Copyright © 2018 Damian Elsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
