8a5e54d167dc2145f78a89c4d436e189796f10db
damianelsen <damian.elsen@googlemail.com>
2018-07-19 21:28:51 -0500

    Added initial list view for Rooms

--- Clea/ViewController.swift
+++ Clea/ViewController.swift
@@ -12,14 +12,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "Rooms"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
+    @IBAction func addRoom(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Room", message: "What is the name of the new room?", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            [unowned self] action in
+            guard let textField = alert.textFields?.first, let nameToSave = textField.text else {
+                return
+            }
+            self.rooms.append(nameToSave)
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var rooms: [String] = []
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,numberOfRowsInSection section: Int) -> Int {
+        return rooms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = rooms[indexPath.row]
+        return cell
+    }
+}
