56ca4df1f0cd31c9484d9690dd21862bcdde1393
damianelsen <damian.elsen@googlemail.com>
2018-07-23 20:34:52 -0500

    Added CoreData persistence for Rooms

--- Clea/ViewController.swift
+++ Clea/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    var rooms: [NSManagedObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,14 +20,28 @@ class ViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Room")
+        do {
+            rooms = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not load rooms. \(error), \(error.userInfo)")
+        }
+    }
+
     @IBAction func addRoom(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "New Room", message: "What is the name of the new room?", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Add Room", message: "What is the name of the new room?", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) {
             [unowned self] action in
             guard let textField = alert.textFields?.first, let nameToSave = textField.text else {
                 return
             }
-            self.rooms.append(nameToSave)
+            self.save(name: nameToSave)
             self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .default)
@@ -34,6 +51,22 @@ class ViewController: UIViewController {
         present(alert, animated: true)
     }
     
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Room", in: managedContext)!
+        let room = NSManagedObject(entity: entity, insertInto: managedContext)
+        room.setValue(name, forKeyPath: "name")
+        do {
+            try managedContext.save()
+            rooms.append(room)
+        } catch let error as NSError {
+            print("Could not add new room. \(error), \(error.userInfo)")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -41,19 +74,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var rooms: [String] = []
-    
 }
 
 extension ViewController: UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView,numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return rooms.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let room = rooms[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = rooms[indexPath.row]
+        cell.textLabel?.text = room.value(forKeyPath: "name") as? String
         return cell
     }
 }
